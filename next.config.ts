0bdfef95f68c0e721f6bf8837c9474be982194bf
Leo <recamaraleojay3@gmail.com>
2026-06-08 20:17:47 +0800

    Force ignore all build checks

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, // Ignores TS errors
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Ignores Lint errors
   },
 };
 
